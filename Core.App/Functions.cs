bd309c0cb2dcfbc5b812100f0272a6733d33bd80
Oscar West <oscar.west@collectorbank.se>
2017-10-02 10:44:42 +0200

    Fix merge conflicts

--- Core.App/Functions.cs
+++ Core.App/Functions.cs
@@ -1,14 +1,14 @@
 using System.IO;
+using Core.Library;
 using Microsoft.Azure.WebJobs;
 
 namespace Core.App
 {
     public class Functions
     {
-        private const string PrefixForAll = "s-";
+        private const string PrefixForAll = "";
         private const string QueueNamePrefix = PrefixForAll + "queue-";
         private const string TopicName = PrefixForAll + "topic";
-
         public const string StartQueueName = QueueNamePrefix + "start";
 
         // Topic subscription listener #1
